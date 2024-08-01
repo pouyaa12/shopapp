037f3cd7c940bd9a442f534cbd19c5d5b80b357c
pouyaa12 <pouyak373@gmail.com>
2024-08-01 12:49:30 +0330

    update navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,14 @@
-function Nav(){
-    return(
-        <div>hi</div>
-    )
+function Nav() {
+  return (
+    <div>
+      <nav>
+        <ul className="m-3 flex">
+          <li className="text-xl font-bold p-3 hover:bg-slate-400 rounded-xl"><a href="#">HOME</a></li>
+          <li className="text-xl p-3 hover:bg-slate-400 rounded-xl"><a href="#">article</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
