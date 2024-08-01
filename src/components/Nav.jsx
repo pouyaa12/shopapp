9608bef453b1a940e1712919f3034306fb2f2aa1
pouyaa12 <pouyak373@gmail.com>
2024-08-01 21:09:12 +0330

    navbar changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,29 @@
 function Nav() {
   return (
-    <div>
-      <nav>
-        <ul className="m-3 flex">
-          <li className="text-xl font-bold p-3 hover:bg-slate-400 rounded-xl"><a href="#">HOME</a></li>
-          <li className="text-xl p-3 hover:bg-slate-400 rounded-xl"><a href="#">article</a></li>
+    <div className="bg-gradient-to-r from-sky-300 to-emerald-500">
+      <nav className="flex items-center justify-between">
+        <ul className="m-1 flex">
+          <li className="text-xl font-bold p-3 hover:bg-slate-100 rounded-xl">
+            <a href="#">HOME</a>
+          </li>
+          <li className="text-xl p-3 hover:bg-slate-300 rounded-xl">
+            <a href="#">article</a>
+          </li>
         </ul>
+        <form role="search">
+          <div>
+            <input
+              type="search"
+              id="mySearch"
+              className="h-9 mr-3 p-4 border-cyan-600 border-2 rounded-lg "
+              placeholder="Search the site…"
+              aria-label="Search through site content"
+            />
+            <button className="h-9 w-20 mr-7 bg-cyan-500 rounded-lg">Search</button>
+          </div>
+        </form>
       </nav>
+      <hr />
     </div>
   );
 }
